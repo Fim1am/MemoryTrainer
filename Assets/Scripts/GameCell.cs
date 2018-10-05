b8e151b12eabcb27a579af011b4c88b2da8afa26
Fim1am <drozhanov@mail.ua>
2018-10-05 20:36:59 +0300

    4

--- Assets/Scripts/GameCell.cs
+++ Assets/Scripts/GameCell.cs
@@ -2,15 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameCell : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
+public class GameCell : MonoBehaviour
+{
+    public 
+	
+	void Start ()
+    {
 		
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
---
+++ Assets/Scripts/NumberBlock.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberBlock : MonoBehaviour
+{
+    [SerializeField]
+    private int maxNum = 100;
+
+    public int BlockNum { get; set; }
+
+	
+	void Start ()
+    {
+        BlockNum = Random.Range(0, maxNum);
+
+        GetComponentInChildren<UnityEngine.UI.Text>().text = BlockNum.ToString();
+	}
+	
+
+}
